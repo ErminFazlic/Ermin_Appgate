eec810cb521fa8d3e50d35b4beab9bd809523ffc
Ermin <Ermin@DESKTOP-A2ME6UC>
2025-04-29 10:12:58 +0200

    Added models needed and setup db context

---
+++ Backend/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using Backend.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend
+{
+    public class Db : DbContext
+    {
+        public Db(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Transaction> Transactions => Set<Transaction>();
+        public DbSet<User> Users => Set<User>();
+    }
+}
